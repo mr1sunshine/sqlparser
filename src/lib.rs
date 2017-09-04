6ba612a938d95ff8f5f64a5e8174851625f0f55d
Alexander Ovchinnikov <Alexander.Ovchinnikof@gmail.com>
2017-09-05 00:23:17 +0300

    Added test cases for whole numeric_literal statement. This numeric literal is implemented as it is decribed in https://sqlite.org/syntax/numeric-literal.html

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,3 @@ extern crate nom;
 
 mod numeric_literal;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
--- src/numeric_literal.rs
+++ src/numeric_literal.rs
@@ -75,7 +75,7 @@ named!(hex_literal<&[u8]>,
 
 named!(numeric_literal<&[u8]>,
     alt!(
-        int_literal | hex_literal
+        hex_literal | int_literal
     )
 );
 
@@ -226,5 +226,348 @@ mod tests {
     fn test_maybe_float_literal_error_1() {
         let res = maybe_float_literal("a".as_bytes()).unwrap_err();
         assert_eq!(res, ErrorKind::Digit);
+    }
+
+    #[test]
+    fn test_first_part_of_int_literal_success_0() {
+        let res = first_part_of_int_literal(".0".as_bytes()).unwrap();
+        assert_eq!(res, (&b""[..], &b".0"[..]));
+    }
+
+    #[test]
+    fn test_first_part_of_int_literal_success_1() {
+        let res = first_part_of_int_literal(".012".as_bytes()).unwrap();
+        assert_eq!(res, (&b""[..], &b".012"[..]));
+    }
+
+    #[test]
+    fn test_first_part_of_int_literal_success_2() {
+        let res = first_part_of_int_literal(".012ab".as_bytes()).unwrap();
+        assert_eq!(res, (&b"ab"[..], &b".012"[..]));
+    }
+
+    #[test]
+    fn test_first_part_of_int_literal_success_3() {
+        let res = first_part_of_int_literal(".42ab".as_bytes()).unwrap();
+        assert_eq!(res, (&b"ab"[..], &b".42"[..]));
+    }
+
+    #[test]
+    fn test_first_part_of_int_literal_success_4() {
+        let res = first_part_of_int_literal("0".as_bytes()).unwrap();
+        assert_eq!(res, (&b""[..], &b"0"[..]));
+    }
+
+    #[test]
+    fn test_first_part_of_int_literal_success_5() {
+        let res = first_part_of_int_literal("012".as_bytes()).unwrap();
+        assert_eq!(res, (&b""[..], &b"012"[..]));
+    }
+
+    #[test]
+    fn test_first_part_of_int_literal_success_6() {
+        let res = first_part_of_int_literal("012ab".as_bytes()).unwrap();
+        assert_eq!(res, (&b"ab"[..], &b"012"[..]));
+    }
+
+    #[test]
+    fn test_first_part_of_int_literal_success_7() {
+        let res = first_part_of_int_literal("42ab".as_bytes()).unwrap();
+        assert_eq!(res, (&b"ab"[..], &b"42"[..]));
+    }
+
+    #[test]
+    fn test_first_part_of_int_literal_success_8() {
+        let res = first_part_of_int_literal("42.0ab".as_bytes()).unwrap();
+        assert_eq!(res, (&b"ab"[..], &b"42.0"[..]));
+    }
+
+    #[test]
+    fn test_first_part_of_int_literal_success_9() {
+        let res = first_part_of_int_literal("42.0".as_bytes()).unwrap();
+        assert_eq!(res, (&b""[..], &b"42.0"[..]));
+    }
+
+    #[test]
+    fn test_first_part_of_int_literal_success_10() {
+        let res = first_part_of_int_literal("42.".as_bytes()).unwrap();
+        assert_eq!(res, (&b"."[..], &b"42"[..]));
+    }
+
+    #[test]
+    fn test_first_part_of_int_literal_success_11() {
+        let res = first_part_of_int_literal("42.a".as_bytes()).unwrap();
+        assert_eq!(res, (&b".a"[..], &b"42"[..]));
+    }
+
+    #[test]
+    fn test_first_part_of_int_literal_error_0() {
+        let res = first_part_of_int_literal("a".as_bytes()).unwrap_err();
+        assert_eq!(res, ErrorKind::Alt);
+    }
+
+    #[test]
+    fn test_first_part_of_int_literal_error_1() {
+        let res = first_part_of_int_literal(".a".as_bytes()).unwrap_err();
+        assert_eq!(res, ErrorKind::Alt);
+    }
+
+    #[test]
+    fn test_first_part_of_int_literal_error_2() {
+        let res = first_part_of_int_literal("".as_bytes()).unwrap_err();
+        assert_eq!(res, ErrorKind::Alt);
+    }
+
+    #[test]
+    fn test_first_part_of_int_literal_error_3() {
+        let res = first_part_of_int_literal(".".as_bytes()).unwrap_err();
+        assert_eq!(res, ErrorKind::Alt);
+    }
+
+    #[test]
+    fn test_first_part_of_int_literal_error_4() {
+        let res = first_part_of_int_literal("".as_bytes()).unwrap_err();
+        assert_eq!(res, ErrorKind::Alt);
+    }
+
+    #[test]
+    fn test_first_part_of_int_literal_error_5() {
+        let res = first_part_of_int_literal("a".as_bytes()).unwrap_err();
+        assert_eq!(res, ErrorKind::Alt);
+    }
+
+    #[test]
+    fn test_second_opt_part_of_int_literal_success_0() {
+        let res = second_opt_part_of_int_literal("42.a".as_bytes()).unwrap();
+        assert_eq!(res, (&b"42.a"[..], &b""[..]));
+    }
+
+    #[test]
+    fn test_second_opt_part_of_int_literal_success_1() {
+        let res = second_opt_part_of_int_literal("E1142.a".as_bytes()).unwrap();
+        assert_eq!(res, (&b".a"[..], &b"E1142"[..]));
+    }
+
+    #[test]
+    fn test_second_opt_part_of_int_literal_success_2() {
+        let res = second_opt_part_of_int_literal("E+1142.a".as_bytes()).unwrap();
+        assert_eq!(res, (&b".a"[..], &b"E+1142"[..]));
+    }
+
+    #[test]
+    fn test_second_opt_part_of_int_literal_success_3() {
+        let res = second_opt_part_of_int_literal("E-1142.a".as_bytes()).unwrap();
+        assert_eq!(res, (&b".a"[..], &b"E-1142"[..]));
+    }
+
+    #[test]
+    fn test_second_opt_part_of_int_literal_success_4() {
+        let res = second_opt_part_of_int_literal("Ea".as_bytes()).unwrap();
+        assert_eq!(res, (&b"Ea"[..], &b""[..]));
+    }
+
+    #[test]
+    fn test_second_opt_part_of_int_literal_success_5() {
+        let res = second_opt_part_of_int_literal("E+a".as_bytes()).unwrap();
+        assert_eq!(res, (&b"E+a"[..], &b""[..]));
+    }
+
+    #[test]
+    fn test_second_opt_part_of_int_literal_success_6() {
+        let res = second_opt_part_of_int_literal("E-a".as_bytes()).unwrap();
+        assert_eq!(res, (&b"E-a"[..], &b""[..]));
+    }
+
+    #[test]
+    fn test_int_literal_success_0() {
+        let res = int_literal("10.1E-10".as_bytes()).unwrap();
+        assert_eq!(res, (&b""[..], &b"10.1E-10"[..]));
+    }
+
+    #[test]
+    fn test_int_literal_success_1() {
+        let res = int_literal("10.1E10".as_bytes()).unwrap();
+        assert_eq!(res, (&b""[..], &b"10.1E10"[..]));
+    }
+
+    #[test]
+    fn test_int_literal_success_2() {
+        let res = int_literal("10.1E+10".as_bytes()).unwrap();
+        assert_eq!(res, (&b""[..], &b"10.1E+10"[..]));
+    }
+
+    #[test]
+    fn test_int_literal_success_3() {
+        let res = int_literal("10E-10".as_bytes()).unwrap();
+        assert_eq!(res, (&b""[..], &b"10E-10"[..]));
+    }
+
+    #[test]
+    fn test_int_literal_success_4() {
+        let res = int_literal("10E10".as_bytes()).unwrap();
+        assert_eq!(res, (&b""[..], &b"10E10"[..]));
+    }
+
+    #[test]
+    fn test_int_literal_success_5() {
+        let res = int_literal("10E+10".as_bytes()).unwrap();
+        assert_eq!(res, (&b""[..], &b"10E+10"[..]));
+    }
+
+    #[test]
+    fn test_int_literal_error_0() {
+        let res = int_literal("a".as_bytes()).unwrap_err();
+        assert_eq!(res, ErrorKind::Alt);
+    }
+
+    #[test]
+    fn test_int_literal_error_1() {
+        let res = int_literal(".a".as_bytes()).unwrap_err();
+        assert_eq!(res, ErrorKind::Alt);
+    }
+
+    #[test]
+    fn test_int_literal_error_2() {
+        let res = int_literal("".as_bytes()).unwrap_err();
+        assert_eq!(res, ErrorKind::Alt);
+    }
+
+    #[test]
+    fn test_int_literal_error_3() {
+        let res = int_literal(".".as_bytes()).unwrap_err();
+        assert_eq!(res, ErrorKind::Alt);
+    }
+
+    #[test]
+    fn test_int_literal_error_4() {
+        let res = int_literal("".as_bytes()).unwrap_err();
+        assert_eq!(res, ErrorKind::Alt);
+    }
+
+    #[test]
+    fn test_int_literal_error_5() {
+        let res = int_literal("a".as_bytes()).unwrap_err();
+        assert_eq!(res, ErrorKind::Alt);
+    }
+
+    #[test]
+    fn test_hex_literal_success_0() {
+        let res = hex_literal("0x1".as_bytes()).unwrap();
+        assert_eq!(res, (&b""[..], &b"0x1"[..]));
+    }
+
+    #[test]
+    fn test_hex_literal_success_1() {
+        let res = hex_literal("0x0AG".as_bytes()).unwrap();
+        assert_eq!(res, (&b"G"[..], &b"0x0A"[..]));
+    }
+
+    #[test]
+    fn test_hex_literal_error_0() {
+        let res = hex_literal("a".as_bytes()).unwrap_err();
+        assert_eq!(res, ErrorKind::Tag);
+    }
+
+    #[test]
+    fn test_hex_literal_error_1() {
+        let res = hex_literal("0xG".as_bytes()).unwrap_err();
+        assert_eq!(res, ErrorKind::HexDigit);
+    }
+
+    #[test]
+    fn test_numeric_literal_success_0() {
+        let res = numeric_literal("10.1E-10".as_bytes()).unwrap();
+        assert_eq!(res, (&b""[..], &b"10.1E-10"[..]));
+    }
+
+    #[test]
+    fn test_numeric_literal_success_1() {
+        let res = numeric_literal("10.1E10".as_bytes()).unwrap();
+        assert_eq!(res, (&b""[..], &b"10.1E10"[..]));
+    }
+
+    #[test]
+    fn test_numeric_literal_success_2() {
+        let res = numeric_literal("10.1E+10".as_bytes()).unwrap();
+        assert_eq!(res, (&b""[..], &b"10.1E+10"[..]));
+    }
+
+    #[test]
+    fn test_numeric_literal_success_3() {
+        let res = numeric_literal("10E-10".as_bytes()).unwrap();
+        assert_eq!(res, (&b""[..], &b"10E-10"[..]));
+    }
+
+    #[test]
+    fn test_numeric_literal_success_4() {
+        let res = numeric_literal("10E10".as_bytes()).unwrap();
+        assert_eq!(res, (&b""[..], &b"10E10"[..]));
+    }
+
+    #[test]
+    fn test_numeric_literal_success_5() {
+        let res = numeric_literal("10E+10".as_bytes()).unwrap();
+        assert_eq!(res, (&b""[..], &b"10E+10"[..]));
+    }
+
+    #[test]
+    fn test_numeric_literal_success_6() {
+        let res = numeric_literal("0x1".as_bytes()).unwrap();
+        assert_eq!(res, (&b""[..], &b"0x1"[..]));
+    }
+
+    #[test]
+    fn test_numeric_literal_success_7() {
+        let res = numeric_literal("0x0AG".as_bytes()).unwrap();
+        assert_eq!(res, (&b"G"[..], &b"0x0A"[..]));
+    }
+
+    #[test]
+    fn test_numeric_literal_success_8() {
+        let res = numeric_literal("0xG".as_bytes()).unwrap();
+        assert_eq!(res, (&b"xG"[..], &b"0"[..]));
+    }
+
+
+    #[test]
+    fn test_numeric_literal_error_0() {
+        let res = numeric_literal("a".as_bytes()).unwrap_err();
+        assert_eq!(res, ErrorKind::Alt);
+    }
+
+    #[test]
+    fn test_numeric_literal_error_1() {
+        let res = numeric_literal(".a".as_bytes()).unwrap_err();
+        assert_eq!(res, ErrorKind::Alt);
+    }
+
+    #[test]
+    fn test_numeric_literal_error_2() {
+        let res = numeric_literal("".as_bytes()).unwrap_err();
+        assert_eq!(res, ErrorKind::Alt);
+    }
+
+    #[test]
+    fn test_numeric_literal_error_3() {
+        let res = numeric_literal(".".as_bytes()).unwrap_err();
+        assert_eq!(res, ErrorKind::Alt);
+    }
+
+    #[test]
+    fn test_numeric_literal_error_4() {
+        let res = numeric_literal("".as_bytes()).unwrap_err();
+        assert_eq!(res, ErrorKind::Alt);
+    }
+
+    #[test]
+    fn test_numeric_literal_error_5() {
+        let res = numeric_literal("a".as_bytes()).unwrap_err();
+        assert_eq!(res, ErrorKind::Alt);
+    }
+
+    #[test]
+    fn test_numeric_literal_error_6() {
+        let res = numeric_literal("a".as_bytes()).unwrap_err();
+        assert_eq!(res, ErrorKind::Alt);
     }
 }
